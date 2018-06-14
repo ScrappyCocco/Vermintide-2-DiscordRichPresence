0ebacc7a2420493916ea39a630157cbc8cfe66dc
Michele <mikelerogeratro@yahoo.it>
2018-06-14 16:29:23 +0200

    Joining is working - Version 1
    First version with Discord Joining operational

--- scripts/mods/DiscordRichVermintide/DiscordRichVermintide.lua
+++ scripts/mods/DiscordRichVermintide/DiscordRichVermintide.lua
@@ -12,6 +12,12 @@ local last_number_of_players = 0 -- Used to store the number of current human pl
 local last_timestamp_saved = 0 -- Used to store the time of begin of the timer
 local last_loading_level_key = "" -- Used to check which level is currently being loaded
 
+local game_started = false -- Used to check if it's the game first start, if yes i need to check if the player want to join because Discord launched the game
+local need_join_on_start = false -- Used to save if the player want to join on launch
+local saved_lobby_id = "" -- The lobby ID to join once the game is started
+
+local current_party_key = "" -- Used to store che current party key, it change everytime the player enter StateIngame, used to have single-use Discord invites
+
 local discord_presence = {
 	details = "Starting the game..."
 }
@@ -23,6 +29,11 @@ local discord_presence = {
 -- Function that return the current timestamp
 local function set_timestamp_to_now()
 	last_timestamp_saved = os.time()
+end
+
+-- Function that return the current player table
+local function get_local_player()
+	return Managers.player:local_player()
 end
 
 -- Function that return the current level key
@@ -37,7 +48,7 @@ end
 
 -- Function that get the player character name
 local function get_player_character_name()
-	return SPProfiles[Managers.player:local_player():profile_index()].character_name
+	return SPProfiles[get_local_player():profile_index()].character_name
 end
 
 -- Function that get and translate the character name
@@ -47,7 +58,7 @@ end
 
 -- Function that get the player career name
 local function get_player_career_name()
-	local player = Managers.player:local_player()
+	local player = get_local_player()
 	return SPProfiles[player:profile_index()].careers[player:career_index()]
 end
 
@@ -61,23 +72,27 @@ local function get_current_number_of_players()
 	return Managers.player:num_human_players()
 end
 
+-- Function that return if the current player is the host
 local function is_current_player_host()
 	return Managers.state.game_mode.is_server
 end
 
+-- Function that return the player ID
 local function get_player_unique_id()
-	return Managers.player:local_player()._unique_id
+	return get_local_player()._unique_id
 end
 
 -- Function that return if the current level is the lobby
 local function is_in_lobby()
-	return get_current_level_key() == "inn_level"
+	return get_local_player().network_manager.matchmaking_manager._ingame_ui.is_in_inn
 end
 
+-- Function that return if the current match is private
 local function is_match_private()
 	return Managers.matchmaking:is_game_private()
 end
 
+-- Function that return the lobby manager based if the player is host or not
 local function get_current_lobby_manager()
 	if is_current_player_host() then
 		return Managers.state.game_mode._lobby_host
@@ -86,8 +101,9 @@ local function get_current_lobby_manager()
 	end
 end
 
-local function get_network_hash()
-	return get_current_lobby_manager().network_hash
+-- Function that create an unique party is that is used to create single-use invitations
+local function update_unique_party_id()
+	current_party_key = get_player_unique_id() .. os.time()
 end
 
 -- Function that return the difficulty localized string
@@ -95,8 +111,9 @@ local function get_difficulty_name()
 	return Localize(DifficultySettings[Managers.state.difficulty.difficulty].display_name)
 end
 
+-- Function that return the current Steam Lobby ID (used to Join)
 local function get_lobby_steam_id()
-	return Steam.id_hex_to_dec(LobbyInternal.lobby_id(get_current_lobby_manager().lobby))
+	return LobbyInternal.lobby_id(get_current_lobby_manager().lobby)
 end
 
 --[[
@@ -111,25 +128,28 @@ end
 -- Update the rich presence details
 local function update_rich_list()
 	local currently = ""
+	local large_image_text = ""
 	local current_lv_key = get_current_level_key()
 	local career_name_translated = get_player_career_name_translated()
 	if is_in_lobby() then
 		currently = "In the lobby"
+		large_image_text = get_level_name(current_lv_key)
 	else
 		currently = "[" .. get_difficulty_name() .. "] " .. get_level_name(current_lv_key)
+		large_image_text = get_difficulty_name() .. " - " .. get_level_name(current_lv_key)
 	end
 	discord_presence = {
 		details = currently,
 		state = "as " .. career_name_translated,
 		largeImageKey = current_lv_key,
-		largeImageText = get_level_name(current_lv_key),
+		largeImageText = large_image_text,
 		smallImageKey = get_player_career_name().display_name,
 		smallImageText = get_player_character_name_translated() .. " - " .. career_name_translated,
-		partyId = get_network_hash(), --TODO, NEED TO BE CHANGED
+		partyId = current_party_key,
 		partySize = last_number_of_players,
 		partyMax = 4,
 		startTimestamp = last_timestamp_saved,
-		joinSecret = get_lobby_steam_id() --DISCUSS ABOUT THIS
+		joinSecret = get_lobby_steam_id()
 	}
 end
 
@@ -137,15 +157,13 @@ end
 	Discord Rich Join Functions
 --]]
 
-function discordRPC.errored(errorCode, message)
-	mod:echo("Error:" .. message)
-    print(string.format("Discord: error (%d: %s)", errorCode, message))
+-- Function that join a game
+local function join_game_with_id(lobby_id)
+	get_local_player().network_manager.matchmaking_manager:request_join_lobby({id=lobby_id,  is_server_invite=false} , { friend_join = true })
 end
 
+-- Discord Callback of joinRequest - Executed when an user press "Ask to Join" on Discord
 function discordRPC.joinRequest(userId, username, discriminator, avatar)
-	print("discordRPC.joinRequest")
-	mod:echo("discordRPC.joinRequest")
-	print(string.format("Discord: join request (%s, %s, %s, %s)", userId, username, discriminator, avatar))
 	if get_current_number_of_players() == 4 or is_match_private() then
 		mod:echo("You automatically refused " .. username .. " join request")
 		discordRPC.respond(userId, "no")
@@ -155,12 +173,15 @@ function discordRPC.joinRequest(userId, username, discriminator, avatar)
 	end
 end
 
+-- Discord Callback of joinGame - Executed when the user Join
 function discordRPC.joinGame(joinSecret)
-	print("discordRPC.joinGame")
-	mod:echo("discordRPC.joinGame")
-	print(string.format("Discord: join (%s)", joinSecret))
-	LobbyInternal.join_lobby(LobbyInternal.get_lobby_data_from_id(joinSecret))
-	-- mod:echo("steam://run/552500// +connect_lobby " .. joinSecret) NOT WORKING
+	mod:echo("Discord RPC - Joining Game...")
+	if not game_started then -- Game not started, save the id for later
+		need_join_on_start = true
+		saved_lobby_id = joinSecret
+	else -- Join now
+		join_game_with_id(joinSecret)
+	end
 end
 
 --[[
@@ -193,6 +214,22 @@ mod:hook_safe(StateSplashScreen, "on_enter", function (...)
 	discordRPC.initialize(appId, true, "552500")
 	update_rich()
 	print("DiscordRichVermintide loaded - ver " .. current_version)
+end)
+
+-- Update Discord RPC when the player is InGame (in lobby/in a mission)
+mod:hook_safe(StateIngame, "on_enter", function (...)
+	if not game_started then -- First start of the game
+		game_started = true
+		if need_join_on_start then -- The player want to Join
+			join_game_with_id(saved_lobby_id)
+			need_join_on_start = false
+		end
+	end
+	--Update Discord Rich Presence
+	set_timestamp_to_now()
+	update_unique_party_id()
+	update_rich_list()
+	update_rich()
 end)
 
 -- Character changed, need to update the discord rich
@@ -249,17 +286,6 @@ end)
 -- dt - time in milliseconds since last update
 mod.update = function(dt)
 	discordRPC.runCallbacks()
-end
-
--- Call when game state changes (e.g. StateLoading -> StateIngame)
-mod.on_game_state_changed = function(status, state)
-	if status == "enter" then
-		if state == "StateIngame" then -- Player has joined a map
-			set_timestamp_to_now()
-			update_rich_list()
-			update_rich()
-		end
-	end
 end
 
 -- Call when all mods are being unloaded, shutdown discord rich
